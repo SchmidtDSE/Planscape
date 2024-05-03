56d0712bd8b1e1c0129368c7a9eda2717f705ece
root <npg@berkeley.edu>
2024-05-03 17:03:12 +0000

    just use latest node and hope it works with our dev container image

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,12 +4,10 @@
 apt-get update
 apt-get install -y curl ca-certificates
 
-# Get setup script for Node.js 14.x from nodesource and run it
-curl -sL https://deb.nodesource.com/setup_14.x | bash -
+# Get setup script for latest Node.js version and install it
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
+sudo apt-get install -y nodejs
 
-# Verify Node.js and npm installation
-node --version
-npm --version
-
-# Install Node.js and npm
-apt-get install -y nodejs
+# cd to the project directory and install dependencies
+cd src/interface
+npm install

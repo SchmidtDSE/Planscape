960628208afca40519a15a64abe03a4c3e01b63f
root <npg@berkeley.edu>
2024-05-03 16:54:20 +0000

    install a newer version of npm that available on ubuntu repositories - need 14+ for chaining operator in node

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Install npm
+# Install curl and ca-certificates first
 apt-get update
-apt-get install -y npm
+apt-get install -y curl ca-certificates
 
-# Verify npm installation
+# Get setup script for Node.js 14.x from nodesource and run it
+curl -sL https://deb.nodesource.com/setup_14.x | bash -
+
+# Verify Node.js and npm installation
+node --version
 npm --version
 
-# Install node packages within src/interface
-cd src/interface
-npm install
+# Install Node.js and npm
+apt-get install -y nodejs

839cb1029df8476793b90a1e5cb7d6e0938a1d0f
GondekNP <npg@berkeley.edu>
2024-05-03 12:21:32 -0700

    okay actually just prefix those init scripts in the base script, so that the subsequent init scripts are run from root rather than their dir

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bash init_scripts/install_dev_dependencies.sh
-bash init_scripts/init_dev_environment.sh
-bash init_scripts/init_dev_dependencies.sh
-bash init_scripts/django_migrate.sh
+bash .devcontainer/init_scripts/install_dev_dependencies.sh
+bash .devcontainer/init_scripts/init_dev_environment.sh
+bash .devcontainer/init_scripts/init_dev_dependencies.sh
+bash .devcontainer/init_scripts/django_migrate.sh

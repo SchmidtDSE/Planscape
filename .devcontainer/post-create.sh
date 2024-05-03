982f0db5ac40d01a50e20ec837281deffafc8512
nick <npg@berkeley.edu>
2024-05-03 10:08:01 -0700

    don't need sudo here

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,8 +5,8 @@ apt-get update
 apt-get install -y curl ca-certificates
 
 # Get setup script for latest Node.js version and install it
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
-sudo apt-get install -y nodejs
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+apt-get install -y nodejs
 
 # cd to the project directory and install dependencies
 cd src/interface

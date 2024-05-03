b3497cded87e8f1413754fd1aa65e4fd2a984a06
root <npg@berkeley.edu>
2024-05-03 16:27:35 +0000

    install npm in post create command to avoid a seperate dev dockerfile ideally

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Install npm
+apt-get update
+apt-get install -y npm
+
+# Verify npm installation
+npm --version
+
+# Install node packages within src/interface
+cd src/interface
+npm install
+npm audit fix
+

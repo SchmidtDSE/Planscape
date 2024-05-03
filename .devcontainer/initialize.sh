55c3c3e743051579ba2cad9e75064936fb8df487
GondekNP <npg@berkeley.edu>
2024-05-03 20:16:29 +0000

    fix typo in path

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -2,5 +2,5 @@
 
 bash .devcontainer/init_scripts/install_dev_dependencies.sh
 bash .devcontainer/init_scripts/init_dev_environment.sh
-bash .devcontainer/init_scripts/init_dev_dependencies.sh
+bash .devcontainer/init_scripts/init_dev_features.sh
 bash .devcontainer/init_scripts/django_migrate.sh
